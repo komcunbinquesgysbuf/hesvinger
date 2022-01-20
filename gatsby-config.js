28585b301497328b2584e288498960530deaa110
komcunbinquesgysbuf <komcunbinquesgysbuf@commodea.com>
2022-01-20 14:35:05 +0000

    Undo extraction of Gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,2 +1,51 @@
-const config = require('./site-config.json');
-module.exports = config;
+module.exports = {
+    siteMetadata: {
+        title: "hesvinger-plain",
+        siteUrl: "https://www.yourdomain.tld"
+    },
+    plugins: [
+        "gatsby-plugin-image",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sharp",
+        "gatsby-remark-images",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-plugin-mdx",
+            options: {
+                extensions: [
+                    ".mdx",
+                    ".md"
+                ],
+                gatsbyRemarkPlugins: [
+                    "gatsby-remark-normalize-paths",
+                    {
+                        resolve: "gatsby-remark-images",
+                        options: {
+                            maxWidth: 1200
+                        }
+                    }
+                ],
+                defaultLayouts: {
+                    pages: require.resolve("./src/components/default-page-layout.js"),
+                    default: require.resolve("./src/components/default-page-layout.js")
+                }
+            }
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/"
+            },
+            __key: "images"
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "pages",
+                path: "./src/pages/"
+            },
+            __key: "pages"
+        }
+    ]
+};
