7c5fb8ea666ee44f8c2a9f4950dcdb0f4a58a13f
komcunbinquesgysbuf <komcunbinquesgysbuf@commodea.com>
2022-01-20 10:19:18 +0000

    Extract config to JSON

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,2 @@
-module.exports = {
-  siteMetadata: {
-      title: `hesvinger-plain`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
-};
+const config = require('./site-config.json');
+module.exports = config;
